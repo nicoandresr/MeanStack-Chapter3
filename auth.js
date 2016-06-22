c546a2fa290e4cfc5eceb9dfd1007c535d472a2e
nicoandresr <nicoandres.rodriguez@gmail.com>
2016-06-22 10:13:16 -0500

    Delete TODO comment

--- auth.js
+++ auth.js
@@ -17,7 +17,6 @@ function setupAuth(User, app) {
   // Facebook-specific
   passport.use(new FacebookStrategy(
     {
-      // TODO: and use the Config service here
       clientID: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
       callbackURL: 'http://localhost:3000/auth/facebook/callback',
